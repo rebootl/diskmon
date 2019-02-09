9f92ff0fe25f446ee62b6876c5865b71c891cdd5
Cem Aydin <cem.aydin@gmx.ch>
2019-02-09 18:58:12 +0100

    server load usage/status (WIP)

--- server.pl
+++ server.pl
@@ -18,7 +18,7 @@
   use Data::Dumper;
 
   use MyConfig;
-  my $config = $MyConfig::config;
+  my @config = @MyConfig::config;
 
   my %dispatch = (
   '/' => \&myresp,
@@ -52,10 +52,73 @@
     }
   }
 
-  sub load_usage2 {
+  # sub load_status {
+  #   my $self = shift;
+  #   # -> maybe use try here
+  #   open FH, '<', $config->{status}->{data_file}
+  #     or die "Could not open file '$config->{status}->{data_file}' $!";
+  #   # data structure:
+  #   # $status = {
+  #   #   /dev/sda => [
+  #   #     { date => ..,
+  #   #       fields => [
+  #   #         { value => <v1>,
+  #   #           limit => <l1> },
+  #   #           ..
+  #   #       ]
+  #   #     },
+  #   #     ..
+  #   #   ],
+  #   #   /dev/sdb => [
+  #   #     ..
+  #   #   ]
+  #   # }
+  #   my $status = {};
+  #   foreach (@{$config->{disks}}) {
+  #     @{$status}{$_} = [];
+  #   }
+  #   while (my $row = <FH>) {
+  #     chomp $row;
+  #     foreach (@{$config->{disks}}) {
+  #       my @tmp = split(/\|/, $row);
+  #       if ($tmp[0] eq $_) {
+  #         shift @tmp;
+  #         my $newhash = { date => shift @tmp };
+  #         my @fields = ();
+  #         foreach (@{$config->{status}->{fields}}) {
+  #           my $value = shift @tmp;
+  #           my $alert = '';
+  #           if ($value eq "no data found") {
+  #             $alert = 'OK';
+  #           } else {
+  #             if ($value >= $_->{'limit'}) {
+  #               $alert = 'ALERT';
+  #             } else {
+  #               $alert = 'OK';
+  #             }
+  #           }
+  #           push @fields, {
+  #             value => $value,
+  #             alert => $alert
+  #           };
+  #           @{$newhash}{fields} = \@fields;
+  #         }
+  #         #print STDERR Dumper($newhash);
+  #         push @{$status->{$_}}, $newhash;
+  #       }
+  #     }
+  #     #print STDERR Dumper($status);
+  #   }
+  #   close FH;
+  #   return $status;
+  # }
+
+  sub load_usage {
     my $self = shift;
-    open FH, '<', $config->{usage}->{data_file}
-      or die "Could not open file '$config->{usage}->{data_file}' $!";
+    my $dev = shift;
+    print STDERR Dumper($dev);
+    open FH, '<', $dev->{usage}->{data_file}
+      or die "Could not open file '$dev->{usage}->{data_file}' $!";
     # improved data struct.:
     # $usage = {
     #   /dev/sda => {
@@ -72,152 +135,10 @@
     #   ...
     #   }
     # }
-    my $usage = {};
-    foreach (@{$config->{disks}}) {
-      ${$usage}{$_} = {
-        dates => [],
-        parts => {},
-      };
-    }
     while (my $row = <FH>) {
       chomp $row;
-      foreach (@{$config->{disks}}) {
-        my @parts = ();
-        my @dates = ();
-        my $run = 0;
-        my @tmp = split(/\|/, $row);
-        if ($tmp[0] eq $_) {
-          shift @tmp;
-          push @{$usage->{$_}->{dates}}, shift @tmp;
-          my %parts = ();
-          while (scalar @tmp >= 2) {
-            if ($run == 0) {
-              ${$usage}{$_}{parts} = [
-                name => shift @tmp,
-                size => [ shift @tmp ],
-                used => [ shift @tmp ],
-              ];
-            }
-          }
-          #  } else {
-          #    push @{$usage->{$_}->{shift @tmp}->{size}}, shift @tmp;
-          #    push @{$usage->{$_}->{shift @tmp}->{usage}}, shift @tmp;
-          #  }
-          #}
-          #@{$newhash}{parts} = \@parts;
-          #push @{$usage->{$_}}, $newhash;
-        }
-      }
+      my @tmp = split(/\|/, $row);
     }
-    print STDERR Dumper($usage);
-    close FH;
-    return $usage;
-  }
-
-  sub load_usage {
-    my $self = shift;
-    open FH, '<', $config->{usage}->{data_file}
-      or die "Could not open file '$config->{usage}->{data_file}' $!";
-    # $usage = {
-    #   /dev/sda =>
-    #     [ { date => <d1>,
-    #         parts => [
-    #          { part => <p1>,
-    #            size => <s1>,
-    #            used => <u1> },
-    #           ..,
-    #     ],
-    #     ..
-    #   }
-    # }
-    my $usage = {};
-    foreach (@{$config->{disks}}) {
-      @{$usage}{$_} = [];
-    }
-    while (my $row = <FH>) {
-      chomp $row;
-      foreach (@{$config->{disks}}) {
-        my @tmp = split(/\|/, $row);
-        if ($tmp[0] eq $_) {
-          shift @tmp;
-          my $newhash = { date => shift @tmp };
-          my @parts = ();
-          while (scalar @tmp >= 2) {
-            push @parts, {
-              part => shift @tmp,
-              size => shift @tmp,
-              used => shift @tmp,
-            };
-          }
-          @{$newhash}{parts} = \@parts;
-          push @{$usage->{$_}}, $newhash;
-        }
-      }
-    }
-    #print STDERR Dumper($usage);
-    close FH;
-    return $usage;
-  }
-
-  sub load_status {
-    my $self = shift;
-    # -> maybe use try here
-    open FH, '<', $config->{status}->{data_file}
-      or die "Could not open file '$config->{status}->{data_file}' $!";
-    # data structure:
-    # $status = {
-    #   /dev/sda => [
-    #     { date => ..,
-    #       fields => [
-    #         { value => <v1>,
-    #           limit => <l1> },
-    #           ..
-    #       ]
-    #     },
-    #     ..
-    #   ],
-    #   /dev/sdb => [
-    #     ..
-    #   ]
-    # }
-    my $status = {};
-    foreach (@{$config->{disks}}) {
-      @{$status}{$_} = [];
-    }
-    while (my $row = <FH>) {
-      chomp $row;
-      foreach (@{$config->{disks}}) {
-        my @tmp = split(/\|/, $row);
-        if ($tmp[0] eq $_) {
-          shift @tmp;
-          my $newhash = { date => shift @tmp };
-          my @fields = ();
-          foreach (@{$config->{status}->{fields}}) {
-            my $value = shift @tmp;
-            my $alert = '';
-            if ($value eq "no data found") {
-              $alert = 'OK';
-            } else {
-              if ($value >= $_->{'limit'}) {
-                $alert = 'ALERT';
-              } else {
-                $alert = 'OK';
-              }
-            }
-            push @fields, {
-              value => $value,
-              alert => $alert
-            };
-            @{$newhash}{fields} = \@fields;
-          }
-          #print STDERR Dumper($newhash);
-          push @{$status->{$_}}, $newhash;
-        }
-      }
-      #print STDERR Dumper($status);
-    }
-    close FH;
-    return $status;
   }
 
   sub myresp {
@@ -225,45 +146,27 @@
     my $cgi = shift;
     return if !ref $cgi;
 
-    my $status = $self->load_status();
-    my $usage = $self->load_usage();
-    my $usage2 = $self->load_usage2();
-    my $usage_json = encode_json $usage;
+    # my $status = $self->load_status();
 
-    # assemble the data diskwise... :(
-    # $data = [
-    #   disk1 => {
-    #     data1 ...
-    #   },
-    #   disk2 => {
-    #     data...
-    #   }
-    # ]
-    # my %data = ();
-    # foreach (@{$config->{disks}}) {
-    #   my @parts = ();
-    #   my $diskdata = {
-    #     status => $status->{$_},
-    #     usage => {
-    #       parts => \@parts,
-    #       json => encode_json $usage->{$_}
-    #     }
-    #   };
-    #   $data{$_} = $diskdata;
-    # }
-    #print STDERR Dumper(\%data);
+    my @data = ();
+    foreach my $dev (@config) {
+      #print STDERR Dumper($dev);
+      my $usage = $self->load_usage($dev);
+      #my $status = load_status($status);
+      push @data, {
+        usage => $usage,
+        #status => $status,
+      };
+    }
 
     my $res;
     my $out = $cgi->header();
     $tt->process(
     "mytemplate.html.tt",
     {
-      # disks => $config->{disks},
-      # result => $res,
-      # config => $config,
-      # status => $status,
-      # usage_json => $usage_json,
-      # data => \%data,
+      config => $config,
+      data => \@data,
+      # -> data json
     },
     \$out,
     ) or die $tt->error;
