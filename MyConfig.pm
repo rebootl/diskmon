40f0739de08f3dd9d9f623985572f71f463859e8
Cem Aydin <cem.aydin@gmx.ch>
2019-02-17 19:19:19 +0100

    cleanup config, css impr., rotate usage text on canv.

--- MyConfig.pm
+++ MyConfig.pm
@@ -6,35 +6,6 @@ package MyConfig;
 use strict;
 use warnings;
 
-our $config2 = {
-  disks => [ '/dev/sda', '/dev/sdb' ],
-  #data_file => 'diskdat.txt',
-  status => {
-    data_file => 'statusdat.txt',
-    # the fields order matter for the file it's written to!!!
-    fields => [
-      {
-        label => 'Reall. Sect. Ct.',
-        name => 'Reallocated_Sector_Ct',
-        limit => 5
-      },
-      {
-        label => 'Airflow Temp. Cel.',
-        name => 'Airflow_Temperature_Cel',
-        limit => 37
-      },
-      {
-        label => 'Curr. Pend. Sect.',
-        name => 'Current_Pending_Sector',
-        limit => 5
-      },
-    ]
-  },
-  usage => {
-    data_file => 'usagedat.txt',
-  }
-};
-
 # list of blockdevices w/ respective parameters
 our @config = (
   {
